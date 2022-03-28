ef291ead6ef6a1bb7cc1f6aeb69558617f12f8a8
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2022-03-28 08:42:54 -0400

    Merge branch 'main' of https://github.com/marcusrein/typescript-discord-bot-sabe.io

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,13 +1,16 @@
 import { Client, ClientOptions } from "discord.js";
-import { token } from "src/secret_token";
+// import { token } from "src/secret_token";
+import ready from "./listeners/ready";
 
-// const token = "OTU3OTU1OTkzNDM2ODQ0MDc0.YkGUCg.DHwmeDeKvyT6laeRLUfq7Dxx_d4";
+const token = "OTU3OTU1OTkzNDM2ODQ0MDc0.YkGUCg.DHwmeDeKvyT6laeRLUfq7Dxx_d4";
 
 console.log("Bot is starting...");
 
 const client = new Client({
 	intents: [],
 });
+
+ready(client);
 
 client.login(token);
 console.log(client);
